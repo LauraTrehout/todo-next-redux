2d733b81fd5c6a95b238a456adca3638b9a000f4
LauraTrehout <lauratrehout@gmail.com>
2021-11-24 14:42:38 +0100

    add todo on keypress

--- components/Todo.js
+++ components/Todo.js
@@ -11,17 +11,18 @@ function Todo({ todos, input, setInput, setTodos, done, setDone, selected, setSe
     setInput(e.target.value)
   }
 
-  const handleAddClick = (e) => {
-    e.preventDefault()
+  const handleAddTodo = (e) => {
+      console.log('key down');
     setTodos([...todos, {title: input, completed: false, id: Math.floor(Math.random()*100)}])
     setInput('')
+    
   }
   return (
     <TodoContainer>
       <AllTasksTitle>Toutes les tâches</AllTasksTitle>
-      <Input type="text" placeholder=" + Ajouter une tâche" value={input} onChange={handleInputChange}/>
-<button onClick={handleAddClick}>+</button>
-      <TodoList todos={todos} done={done} setDOne={setDone} selected={selected}
+      <Input type="text" placeholder=" + Ajouter une tâche" value={input} onChange={handleInputChange} onKeyDown={(e) => e.key === 'Enter' && handleAddTodo()}/>
+{/* <button onClick={handleAddClick}>+</button> */}
+      <TodoList todos={todos} done={done} setDone={setDone} selected={selected}
               setSelected={setSelected}/>
     </TodoContainer>
   );
